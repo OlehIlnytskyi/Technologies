eac0e58544a570740c684f8b8998586892fdf453
Oleg Ilnystkyi <oleh.ilnytskyi.knm.2020@lpnu.ua>
2024-01-22 16:27:25 +0200

    Connected everything at basic level

--- Spring Cloud Config/config-client/src/main/java/com/example/configclient/ConfigClientApplication.java
+++ Spring Cloud Config/config-client/src/main/java/com/example/configclient/ConfigClientApplication.java
@@ -1,13 +1,24 @@
 package com.example.configclient;
 
+import lombok.extern.java.Log;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Log
 @SpringBootApplication
-public class ConfigClientApplication {
+public class ConfigClientApplication implements CommandLineRunner {
+
+	@Value("${vars.variable_one.value}")
+	private String testValue;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigClientApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		log.info("Test value - " + testValue);
+	}
 }
