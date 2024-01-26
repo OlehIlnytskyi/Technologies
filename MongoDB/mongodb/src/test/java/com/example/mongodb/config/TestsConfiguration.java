5c55c2f4c683338dea2290d97c89d2d967bd0cdf
Oleg Ilnystkyi <oleh.ilnytskyi.knm.2020@lpnu.ua>
2024-01-26 21:35:23 +0200

    Enabled Transactions for tests

--- MongoDB/mongodb/src/test/java/com/example/mongodb/config/TestsConfiguration.java
+++ MongoDB/mongodb/src/test/java/com/example/mongodb/config/TestsConfiguration.java
@@ -1,12 +1,16 @@
 package com.example.mongodb.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.MongoDatabaseFactory;
+import org.springframework.data.mongodb.MongoTransactionManager;
 import org.testcontainers.containers.MongoDBContainer;
 import org.testcontainers.junit.jupiter.Container;
 
 @Configuration
-public class MongoDBTestContainerConfig {
+public class TestsConfiguration {
 
+    //  Test MongoDB container
     @Container
     public static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:6.0.13")
             .withExposedPorts(27017);
@@ -15,5 +19,11 @@ public class MongoDBTestContainerConfig {
         mongoDBContainer.start();
         var mappedPort = mongoDBContainer.getMappedPort(27017);
         System.setProperty("mongodb.container.port", String.valueOf(mappedPort));
+    }
+
+    //  We need that to enable Transactions for tests
+    @Bean
+    MongoTransactionManager transactionManager(MongoDatabaseFactory dbFactory) {
+        return new MongoTransactionManager(dbFactory);
     }
 }
--- MongoDB/mongodb/src/test/java/com/example/mongodb/repository/StudentRepositoryTests.java
+++ MongoDB/mongodb/src/test/java/com/example/mongodb/repository/StudentRepositoryTests.java
@@ -1,35 +1,39 @@
 package com.example.mongodb.repository;
 
-import com.example.mongodb.config.MongoDBTestContainerConfig;
+import com.example.mongodb.config.TestsConfiguration;
 import com.example.mongodb.model.Student;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
-import org.springframework.boot.test.context.SpringBootTestContextBootstrapper;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
-import org.springframework.test.context.BootstrapWith;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.transaction.annotation.Transactional;
-import org.testcontainers.containers.MongoDBContainer;
-import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@Transactional
 @DataMongoTest
 @Testcontainers
-@ContextConfiguration(classes = MongoDBTestContainerConfig.class)
-@EnableMongoRepositories
+@ContextConfiguration(classes = TestsConfiguration.class)
+@EnableMongoRepositories(basePackages = "com.example.mongodb.repository")
 public class StudentRepositoryTests {
 
     @Autowired
     private StudentRepository studentRepository;
 
     @Test
-    void test() {
+    void testSaveMethodWorks() {
         Student student = getTestStudent_A();
+
+        studentRepository.save(student);
+
+        assertEquals(1, studentRepository.count());
+    }
+
+    @Test
+    void testSaveMethodWorksAgain() {
+        Student student = getTestStudent_B();
 
         studentRepository.save(student);
 
@@ -41,6 +45,14 @@ public class StudentRepositoryTests {
                 .name("Student")
                 .surname("A")
                 .grade(3)
+                .build();
+    }
+
+    public Student getTestStudent_B() {
+        return Student.builder()
+                .name("Student")
+                .surname("B")
+                .grade(4)
                 .build();
     }
 }
