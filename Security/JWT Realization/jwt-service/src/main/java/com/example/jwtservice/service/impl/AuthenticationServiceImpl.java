6d960779f153c3e7ec9a30de999cd08aa31fcc22
Oleg Ilnystkyi <oleh.ilnytskyi.knm.2020@lpnu.ua>
2024-03-16 22:31:32 +0200

    Implemented Login functionality

--- Security/JWT Realization/jwt-service/src/main/java/com/example/jwtservice/service/impl/AuthenticationServiceImpl.java
+++ Security/JWT Realization/jwt-service/src/main/java/com/example/jwtservice/service/impl/AuthenticationServiceImpl.java
@@ -11,11 +11,14 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.Objects;
+
 @Log
 @Service
 public class AuthenticationServiceImpl implements AuthenticationService {
 
-    private final static String USER_ALREADY_IN_DATABASE = "There is already user with username: ";
+    private final static String USER_ALREADY_IN_DATABASE_RESPONSE = "There is already user with username: ";
+    private final static String INCORRECT_USERNAME_OR_PASSWORD_RESPONSE = "Incorrect username or password!";
 
     private final UserDetailsRepository userDetailsRepository;
 
@@ -26,7 +29,7 @@ public class AuthenticationServiceImpl implements AuthenticationService {
 
     @Override
     public ResponseEntity<String> register(RegisterRequest request) {
-        UserDetails userDetails = requestToUserDetails(request);
+        UserDetails userDetails = registerRequestToUserDetails(request);
 
         if (isUserPresentInDatabase(userDetails))
             return userAlreadyInDatabaseResponse(userDetails);
@@ -37,7 +40,14 @@ public class AuthenticationServiceImpl implements AuthenticationService {
 
     @Override
     public ResponseEntity<String> login(LoginRequest request) {
-        return null;
+        UserDetails userDetails = loginRequestToUserDetails(request);
+        UserDetails savedUserDetails = getUserDetailsOrBlank(userDetails);
+
+        if (!arePasswordsEquals(userDetails, savedUserDetails))
+            // This will also be triggered if the username is incorrect
+            return incorrectUsernameOrPasswordResponse();
+
+        return jwtTokenResponse(userDetails);
     }
 
     @Override
@@ -46,7 +56,14 @@ public class AuthenticationServiceImpl implements AuthenticationService {
     }
 
 
-    private UserDetails requestToUserDetails(RegisterRequest request) {
+    private UserDetails registerRequestToUserDetails(RegisterRequest request) {
+        return UserDetails.builder()
+                .username(request.username)
+                .password(request.password)
+                .build();
+    }
+
+    private UserDetails loginRequestToUserDetails(LoginRequest request) {
         return UserDetails.builder()
                 .username(request.username)
                 .password(request.password)
@@ -57,18 +74,34 @@ public class AuthenticationServiceImpl implements AuthenticationService {
         return userDetailsRepository.findByUsername(userDetails.getUsername()).isPresent();
     }
 
+    private boolean arePasswordsEquals(UserDetails userDetails1, UserDetails userDetails2) {
+        return Objects.equals(userDetails1.getPassword(), userDetails2.getPassword());
+    }
+
+    private UserDetails getUserDetailsOrBlank(UserDetails userDetails) {
+        return userDetailsRepository.findByUsername(userDetails.getUsername())
+                .orElse(new UserDetails());
+    }
+
     private void saveUserDetails(UserDetails userDetails) {
         userDetailsRepository.save(userDetails);
     }
 
     private ResponseEntity<String> userAlreadyInDatabaseResponse(UserDetails userDetails) {
-        return ResponseEntity.status(HttpStatus.CONFLICT)
-                .body(USER_ALREADY_IN_DATABASE + userDetails.getUsername());
+        String body = USER_ALREADY_IN_DATABASE_RESPONSE + userDetails.getUsername();
+        return response(HttpStatus.UNAUTHORIZED, body);
+    }
+
+    private ResponseEntity<String> incorrectUsernameOrPasswordResponse() {
+        return response(HttpStatus.UNAUTHORIZED, INCORRECT_USERNAME_OR_PASSWORD_RESPONSE);
     }
 
     private ResponseEntity<String> jwtTokenResponse(UserDetails userDetails) {
         String jwtToken = JwtUtils.generateToken(userDetails);
-        return ResponseEntity.status(HttpStatus.FOUND)
-                .body(jwtToken);
+        return response(HttpStatus.OK, jwtToken);
+    }
+
+    private <T> ResponseEntity<T> response(HttpStatus status, T body) {
+        return ResponseEntity.status(status).body(body);
     }
 }
